5b5403c1538d3923d36bef6f1e9c05d09865c543
Kimberly Nicholls <kimberly@gridium.com>
2017-09-25 22:00:00 -0700

    export data from AirTable to a JSON on S3 via AWS Lambda

---
+++ README.md
@@ -0,0 +1,38 @@
+# export Airtable data to JSON
+
+AirTable has an API, but offers just one full access API key. Obviously this can't be used in a user-facing app. This repo has directions and a script to fetch data from an AirTable API using an AWS Lambda function and save it to a JSON file on S3.
+
+## setup
+
+install dependencies for local testing:
+
+    pip install -e requirements.txt
+
+install dependencies for deployment package:
+
+    pip install requests -t /path/to/project-dir
+
+## AWS setup
+
+- create an S3 bucket (see http://docs.aws.amazon.com/AmazonS3/latest/gsg/CreatingABucket.html)
+- create a new policy that references the bucket (TODO: example)
+- make a Lambda role that references the policy (see http://docs.aws.amazon.com/lambda/latest/dg/with-s3-example-create-iam-role.html)
+- create an AWS Lambda function (see http://docs.aws.amazon.com/lambda/latest/dg/get-started-create-function.html)
+- assign the role to the Lambda function (TODO: example)
+- create schedule to update output file (see http://docs.aws.amazon.com/AmazonCloudWatch/latest/events/ScheduledEvents.html)
+- create Lambda deployment package:
+
+    zip -q -r aws/lambda.zip . -x aws/* *.git*
+
+- upload `aws/lambda.zip` to AWS
+- set required environment variables for Lambda function:
+    - `AIRTABLE_APP` - AirTable app id (from https://airtable.com/api)
+    - `AIRTABLE_TABLE` - AirTable table id (from https://airtable.com/api)
+    - `AIRTABLE_TOKEN` - API token (from https://airtable.com/api)
+    - `S3_BUCKET` - destination bucket
+- set optional environment variables for Lambda function if needed (will use defaults if not set):
+    - `S3_FILENAME` - destination filename (default airtable.json)
+    - `ACL` - access control for S3 file (default public-read)
+    - `CACHE_HOURS` - set Expires header to this many hours in future (default 6)
+- test the Lambda function; verify it creates `S3_BUCKET/S3_FILENAME`
+- use the JSON file in your app
---
+++ lambda.py
@@ -0,0 +1,73 @@
+'''
+    fetch data from AirTable API and write to a file on S3
+
+    set as environment variables for Lambda function
+
+    required:
+    - AIRTABLE_APP - AirTable app id (from https://airtable.com/api)
+    - AIRTABLE_TABLE - AirTable table id (from https://airtable.com/api)
+    - AIRTABLE_TOKEN - API token (from https://airtable.com/api)
+    - S3_BUCKET - destination bucket
+
+    optional (will use defaults if not set):
+    - S3_FILENAME - destination filename (default airtable.json)
+    - ACL - access control for S3 file (default public-read)
+    - CACHE_HOURS - set Expires header to this many hours in future (default 6)
+'''
+
+from datetime import datetime, timedelta
+import json
+import os
+
+import boto3
+import requests
+
+API_URL = 'https://api.airtable.com/v0/%s/%s' % (
+    os.environ['AIRTABLE_APP'], os.environ['AIRTABLE_TABLE'])
+AUTH = {'Authorization': 'Bearer %s' % os.environ['AIRTABLE_TOKEN']}
+FILENAME = os.environ.get('S3_FILENAME', 'airtable.json')
+BUCKET = os.environ['S3_BUCKET']
+
+def load_data():
+    '''
+        load data from AirTable, fetching additional pages if needed
+    '''
+    rval = {'updated': datetime.now().isoformat()}
+    params = {}
+    req = requests.get(API_URL, headers=AUTH, params=params)
+    rval = req.json()
+    data = req.json()
+    while data.get('offset'):
+        params['offset'] = data['offset']
+        req = requests.get(API_URL, headers=AUTH, params=params)
+        print('offset=%s url=%s request=%s' % (params['offset'], API_URL, req))
+        data = req.json()
+        rval['records'] += data.get('records', [])
+    print('loaded %s records' % (len(rval.get('records', []))))
+    return rval
+
+
+def write_json(data):
+    '''
+        write to S3 bucket
+    '''
+    s3 = boto3.resource('s3')
+    print('put %s/%s' % (BUCKET, FILENAME))
+    hours = int(os.environ.get('CACHE_HOURS', 6))
+    expires_dt = (datetime.now() + timedelta(hours=hours))
+    s3.Object(BUCKET, FILENAME).put(
+        Body=json.dumps(data),
+        ContentType='application/json',
+        ACL=os.environ.get('ACL', 'public-read'),
+        Expires=expires_dt
+    )
+
+
+def lambda_handler(event, context):
+    write_json(load_data())
+
+
+if __name__ == '__main__':
+    print('get data from %s and write to %s/%s' % (API_URL, BUCKET, FILENAME))
+    write_json(load_data())
+
